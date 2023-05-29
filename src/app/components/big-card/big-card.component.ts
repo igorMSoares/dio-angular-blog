0c72d12f932b0fa3f57afc200b318a6f7487c43a
igorMSoares <igorms.dev@gmail.com>
2023-05-29 14:32:47 -0300

    feat: small-card and big-card receiving props

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent {
+  @Input()
+  photoCover: string = '';
 
+  @Input()
+  cardDate: string = '';
+
+  @Input()
+  cardTitle: string = '';
+
+  @Input()
+  cardAnchorURL: string = ''
+
+  @Input()
+  cardDescription: string = '';
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent {
+  @Input()
+  photoCover: string = '';
 
+  @Input()
+  cardDate: string = '';
+
+  @Input()
+  cardTitle: string = '';
+
+  @Input()
+  cardAnchorURL: string = '';
+
+  @Input()
+  cardDescription: string = '';
 }

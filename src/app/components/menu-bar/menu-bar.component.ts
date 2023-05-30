6e76032f442cb668311ff3d5479499523d3cdc01
igorMSoares <igorms.dev@gmail.com>
2023-05-29 22:42:34 -0300

    feat: add Home menu link when not on home page

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css']
+  styleUrls: ['./menu-bar.component.css'],
 })
 export class MenuBarComponent {
+  @Input()
+  isHomePage: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(val => {
+      if (val instanceof NavigationEnd)
+        this.isHomePage = val.url === '/';
+    });
+  }
 }
